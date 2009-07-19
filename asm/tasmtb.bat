52410dd6fb2cefadfbda477aac2867f51096aaa0
ja_rd <ja_rd@a7862649-a01a-4c5c-903c-bc397d088372>
2009-07-19 20:29:49 +0000

    test bench compilation script sanitized a little

--- asm/tasmtb.bat
+++ asm/tasmtb.bat
@@ -6,9 +6,8 @@
 @rem remove output from previous assembly
 @del %PROG%.hex
 @del %PROG%.lst
-@rem bring TASM 8080 table file to current directory or TASM will fail
-@copy %TASM_DIR%\tasm85.tab .
+@rem make sure TASM is able to find its table files (see TASM documentation)
+@set TASMTABS=..\local\tasm
 %TASM_DIR%\tasm -85 -a %PROG%.asm %PROG%.hex %PROG%.lst
 @rem 
-@rem FIXME template needs an overhaul
 @perl hexconv.pl .\%PROG%.hex ..\vhdl\test\tb_template.vhdl 000 800 > ..\vhdl\test\light8080_%PROG%.vhdl
